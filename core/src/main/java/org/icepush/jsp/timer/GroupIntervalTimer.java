26cb9cc185c3c61c1ba6e4fd57604001b1b7707e
mircea.toma <mircea.toma@1de00972-ac04-4911-a46b-06793c8d78d9>
2013-01-17 14:00:30 +0000

    PUSH-209 Updated copyright headers.
    git-svn-id: http://anonsvn.icesoft.org/repo/icepush/trunk/icepush@33088 1de00972-ac04-4911-a46b-06793c8d78d9

--- core/src/main/java/org/icepush/jsp/timer/GroupIntervalTimer.java
+++ core/src/main/java/org/icepush/jsp/timer/GroupIntervalTimer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2012 ICEsoft Technologies Canada Corp.
+ * Copyright 2004-2013 ICEsoft Technologies Canada Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the
