12da6bf3eea624489018a4a7c25eaa0c2ece435a
mircea.toma <mircea.toma@1de00972-ac04-4911-a46b-06793c8d78d9>
2013-06-06 10:31:39 +0000

    ICE-7183 Updated bridge-support files with versions that the have ice.lib.configuration module included.
    git-svn-id: http://anonsvn.icesoft.org/repo/icepush/trunk/icepush@35977 1de00972-ac04-4911-a46b-06793c8d78d9

--- core/src/main/resources/META-INF/ice.core/bridge-support.uncompressed.js
+++ core/src/main/resources/META-INF/ice.core/bridge-support.uncompressed.js
@@ -716,6 +716,102 @@ ice.lib.collection = ice.module(function(exportAs) {
     exportAs('Cell', Cell);
     exportAs('Stream', Stream);
 });
+ice.lib.configuration = ice.module(function(exportAs) {
+    eval(ice.importFrom('ice.lib.oo'));
+    eval(ice.importFrom('ice.lib.string'));
+    eval(ice.importFrom('ice.lib.collection'));
+    var attributeAsString = operator();
+    var attributeAsBoolean = operator();
+    var attributeAsNumber = operator();
+    var valueAsStrings = operator();
+    var valueAsBooleans = operator();
+    var valueAsNumbers = operator();
+    var childConfiguration = operator();
+    function XMLDynamicConfiguration(lookupElement) {
+        function asBoolean(s) {
+            return 'true' == toLowerCase(s);
+        }
+        function lookupAttribute(name) {
+            var a = lookupElement().getAttribute(name);
+            if (a) {
+                return a;
+            } else {
+                throw 'unknown attribute: ' + name;
+            }
+        }
+        function lookupValues(name) {
+            return collect(asArray(lookupElement().getElementsByTagName(name)), function(e) {
+                var valueNode = e.firstChild;
+                return valueNode ? valueNode.nodeValue : '';
+            });
+        }
+        return object(function(method) {
+            method(attributeAsString, function(self, name, defaultValue) {
+                try {
+                    return lookupAttribute(name);
+                } catch (e) {
+                    if (isString(defaultValue)) {
+                        return defaultValue;
+                    } else {
+                        throw e;
+                    }
+                }
+            });
+            method(attributeAsNumber, function(self, name, defaultValue) {
+                try {
+                    return Number(lookupAttribute(name));
+                } catch (e) {
+                    if (isNumber(defaultValue)) {
+                        return defaultValue;
+                    } else {
+                        throw e;
+                    }
+                }
+            });
+            method(attributeAsBoolean, function(self, name, defaultValue) {
+                try {
+                    return asBoolean(lookupAttribute(name));
+                } catch (e) {
+                    if (isBoolean(defaultValue)) {
+                        return defaultValue;
+                    } else {
+                        throw e;
+                    }
+                }
+            });
+            method(childConfiguration, function(self, name) {
+                var elements = lookupElement().getElementsByTagName(name);
+                if (isEmpty(elements)) {
+                    throw 'unknown configuration: ' + name;
+                } else {
+                    return XMLDynamicConfiguration(function() {
+                        return lookupElement().getElementsByTagName(name)[0];
+                    });
+                }
+            });
+            method(valueAsStrings, function(self, name, defaultValues) {
+                var values = lookupValues(name);
+                return isEmpty(values) && defaultValues ? defaultValues : values;
+            });
+            method(valueAsNumbers, function(self, name, defaultValues) {
+                var values = lookupValues(name);
+                return isEmpty(values) && defaultValues ? defaultValues : collect(values, Number);
+            });
+            method(valueAsBooleans, function(self, name, defaultValues) {
+                var values = lookupValues(name);
+                return isEmpty(values) && defaultValues ? defaultValues : collect(values, asBoolean);
+            });
+        });
+    }
+    exportAs('attributeAsString', attributeAsString);
+    exportAs('attributeAsBoolean', attributeAsBoolean);
+    exportAs('attributeAsNumber', attributeAsNumber);
+    exportAs('valueAsStrings', valueAsStrings);
+    exportAs('valueAsBooleans', valueAsBooleans);
+    exportAs('valueAsNumbers', valueAsNumbers);
+    exportAs('childConfiguration', childConfiguration);
+    exportAs('XMLDynamicConfiguration', XMLDynamicConfiguration);
+});
 ice.lib.window = ice.module(function(exportAs) {
     eval(ice.importFrom('ice.lib.functional'));
     function registerListener(eventType, obj, listener) {
