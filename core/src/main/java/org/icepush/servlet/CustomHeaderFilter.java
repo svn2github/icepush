062ea3f91f5c35c2a38a5daaebbd5a800554aa1a
ted.goddard <ted.goddard@1de00972-ac04-4911-a46b-06793c8d78d9>
2013-09-11 17:06:28 +0000

    simple basic push service (NTFY-2)
    git-svn-id: http://anonsvn.icesoft.org/repo/icepush/trunk/icepush@37848 1de00972-ac04-4911-a46b-06793c8d78d9

---
+++ core/src/main/java/org/icepush/servlet/CustomHeaderFilter.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2004-2013 ICEsoft Technologies Canada Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS
+ * IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.icepush.servlet;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+
+public class CustomHeaderFilter implements Filter {
+
+    private FilterConfig filterConfig = null;
+
+    public void init(FilterConfig filterConfig)
+            throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+
+    public void destroy() {
+        this.filterConfig = null;
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, 
+            FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpResp = (HttpServletResponse) resp;
+        HttpServletRequest httpRequest = (HttpServletRequest) req;
+
+        Enumeration headerNames = filterConfig.getInitParameterNames();
+        while (headerNames.hasMoreElements())  {
+            String headerName = (String) headerNames.nextElement();
+            String headerValue = filterConfig.getInitParameter(headerName);
+            if ("Access-Control-Allow-Origin".equalsIgnoreCase(headerName)
+                && "*".equals(headerValue))  {
+                String origin = httpRequest.getHeader("Origin");
+                httpResp.addHeader("Access-Control-Allow-Origin", 
+                        origin);
+            } else {
+                httpResp.addHeader(headerName, headerValue);
+            }
+        }
+
+        String method = httpRequest.getMethod();
+
+        //hack to not pass OPTIONS requests to ICEpush
+        if ("OPTIONS".equalsIgnoreCase(method))  {
+            return;
+        }
+
+        chain.doFilter(req, resp);
+    }
+}
+
