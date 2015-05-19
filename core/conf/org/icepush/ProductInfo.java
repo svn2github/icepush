8b0350ed8e969f943a475690efceb7dd533b1e45
jack.van.ooststroom <jack.van.ooststroom@8668f098-c06c-11db-ba21-f49e70c34f74>
2015-05-19 18:21:44 +0000

    Fixed JIRA ICE-10691 : Remove public void main(...) methods from ProductInfo classes
    git-svn-id: http://anonsvn.icesoft.org/repo/icepush/trunk/icepush@44711 8668f098-c06c-11db-ba21-f49e70c34f74

--- core/conf/org/icepush/ProductInfo.java
+++ core/conf/org/icepush/ProductInfo.java
@@ -83,9 +83,4 @@ public class ProductInfo {
         info.append("\n");
         return info.toString();
     }
-
-    public static void main(String[] args) {
-        ProductInfo app = new ProductInfo();
-        System.out.println(app.toString());
-    }
 }
